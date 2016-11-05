b3932bf2eb4ca7599fa26b2492f88713e6de0f44
NCordon <nacho.cordon.castillo@gmail.com>
2016-11-05 04:05:57 +0100

    Final version

--- dijkstra/dijkstra.cc
+++ dijkstra/dijkstra.cc
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <utility>
 #include <vector>
+#include <list>
 #include <queue>
 #include <limits>
 
@@ -16,7 +17,10 @@ public:
   }
 };
 
-vector< int > solve_dijkstra(int start, vector< vector<int> > &weight, int &num_nodes){  
+vector< int > solve_dijkstra(int start,
+                             vector< list<int> > &graph,
+                             vector< vector<int> > &weight,
+                             int &num_nodes){  
   vector<int> distance(num_nodes, inf);
   vector<bool> visited(num_nodes, false);
   priority_queue< pair<int,int>, vector< pair<int,int> >, edge_order> to_visit;
@@ -31,15 +35,20 @@ vector< int > solve_dijkstra(int start, vector< vector<int> > &weight, int &num_
     to_visit.pop();
     visited[u] = true;
 
-    for(int v=0; v < num_nodes; v++){
+    for(int v : graph[u]){
       // If node hasn't been visited yet, vertex is adjacent and distance can be improved
-      if(!visited[v] and (weight[u][v] != inf) and (distance[v] > (dist + weight[u][v]) )){
+      if(distance[v] > (dist + weight[u][v])){
         distance[v] = (distance[u] + weight[u][v]);
         to_visit.push( make_pair(v, distance[v]) );
       }
+
+      graph[v].remove(u);
+      
     }
+    
   }
 
+  replace(distance.begin(), distance.end(), inf, -1);
   return distance;
 }
 
@@ -50,30 +59,36 @@ int main(){
     num_edges,
     u, v, dist,
     start;
-  cin >> num_cases;
 
+  std::ios_base::sync_with_stdio(false);
+
+  cin >> num_cases;
+  
   // Read a graph and calculate minimum distances to each node
   for (int i=0; i<num_cases; i++){
     cin >> num_nodes >> num_edges;
 
     vector< vector<int> > weight(num_nodes, vector<int>(num_nodes, inf));
     vector<int> distance;
+    vector< list<int> > graph(num_nodes, list<int>());
     
     for(int j=0; j<num_edges; j++){
       // Read pair of nodes and distance between them
       cin >> u >> v >> dist;
       u--; v--;
-
+      graph[u].push_back(v);
+      graph[v].push_back(u);
+      
       if (weight[u][v] > dist)
         weight[u][v] = weight[v][u] = dist;      
     }
     
     cin >> start; start--;
-    distance = solve_dijkstra(start, weight, num_nodes);
-
-    for (int d : distance)
-      if(d!=0)
-        cout << (d==inf ? -1 : d) << " ";
+    distance = solve_dijkstra(start, graph, weight, num_nodes);
+    
+    for(vector<int>::iterator it=distance.begin(); it!=distance.end(); it++)
+      if (*it!=0)
+        cout << *it << " ";
 
     cout << endl;
   }
