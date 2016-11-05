f1e721b11fc211d5470f01625d7ace834d32ea8a
NCordon <nacho.cordon.castillo@gmail.com>
2016-11-05 02:47:22 +0100

    Dijkstra working although timing out

--- dijkstra/dijkstra.cc
+++ dijkstra/dijkstra.cc
@@ -9,11 +9,17 @@
 
 using namespace std;
 
+class edge_order{
+public:
+  bool operator()(const pair<int,int> &u, const pair<int,int> v){
+    return u.second > v.second;
+  }
+};
 
 vector< int > solve_dijkstra(int start, vector< vector<int> > &weight, int &num_nodes){  
   vector<int> distance(num_nodes, inf);
   vector<bool> visited(num_nodes, false);
-  priority_queue< pair<int,int> > to_visit;
+  priority_queue< pair<int,int>, vector< pair<int,int> >, edge_order> to_visit;
   int u, dist;
   
   distance[start] = 0;
@@ -26,9 +32,9 @@ vector< int > solve_dijkstra(int start, vector< vector<int> > &weight, int &num_
     visited[u] = true;
 
     for(int v=0; v < num_nodes; v++){
-
-      if(!visited[v] and (distance[v] > (distance[u] + weight[v][u]) )){
-        distance[v] = (distance[u] + weight[v][u]);
+      // If node hasn't been visited yet, vertex is adjacent and distance can be improved
+      if(!visited[v] and (weight[u][v] != inf) and (distance[v] > (dist + weight[u][v]) )){
+        distance[v] = (distance[u] + weight[u][v]);
         to_visit.push( make_pair(v, distance[v]) );
       }
     }
@@ -48,17 +54,15 @@ int main(){
 
   // Read a graph and calculate minimum distances to each node
   for (int i=0; i<num_cases; i++){
-    cin >> num_nodes;
-    cin >> num_edges;
+    cin >> num_nodes >> num_edges;
 
     vector< vector<int> > weight(num_nodes, vector<int>(num_nodes, inf));
     vector<int> distance;
     
     for(int j=0; j<num_edges; j++){
       // Read pair of nodes and distance between them
-      cin >> u; u--;
-      cin >> v; v--;
-      cin >> dist;
+      cin >> u >> v >> dist;
+      u--; v--;
 
       if (weight[u][v] > dist)
         weight[u][v] = weight[v][u] = dist;      
@@ -70,5 +74,7 @@ int main(){
     for (int d : distance)
       if(d!=0)
         cout << (d==inf ? -1 : d) << " ";
+
+    cout << endl;
   }
 }
