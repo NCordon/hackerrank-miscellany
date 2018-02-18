0ecc498cf2c10b9b91a9dd1468f98899045ac360
ncordon <nacho.cordon.castillo@gmail.com>
2018-02-18 20:54:27 +0100

    Minimum consecutive genes modification problem

---
+++ strings/gene-modification.cc
@@ -0,0 +1,93 @@
+// https://www.hackerrank.com/challenges/bear-and-steady-gene/problem
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct GeneCount{
+  long A; 
+  long C;
+  long G;
+  long T;  
+};
+
+bool operator<(const GeneCount& count_one, const GeneCount& count_two) {
+  if (count_one.A < count_two.A ||
+      count_one.C < count_two.C ||
+      count_one.G < count_two.G ||
+      count_one.T < count_two.T)
+    return true;
+  else
+    return false;
+}
+
+GeneCount operator-(const GeneCount& count_one, const GeneCount& count_two) {
+  GeneCount result;
+  result.A = count_one.A - count_two.A;
+  result.C = count_one.C - count_two.C;
+  result.G = count_one.G - count_two.G;
+  result.T = count_one.T - count_two.T;
+  
+  return result;
+}
+
+void UpdateCount(GeneCount& current, char g) {
+  if (g == 'A')
+    current.A++;
+  else if (g == 'C')
+    current.C++;
+  else if (g == 'G')
+    current.G++;
+  else if (g == 'T')
+    current.T++;
+}
+
+long ShortestModification(string genes, int n) {
+  long result = n;
+  vector<GeneCount> counts(n + 1);
+  long right = 1;
+  counts[0].A = counts[0].C = counts[0].G = counts[0].T = 0;
+  
+  // Compute cumulative counts of each gene
+  for (long i = 1; i <= n; i++) {
+    counts[i] = counts[i - 1];
+    UpdateCount(counts[i], genes[i - 1]);
+  }
+  
+  GeneCount target;
+  target.A = max(counts[n].A - n / 4, 0L);
+  target.C = max(counts[n].C - n / 4, 0L);  
+  target.G = max(counts[n].G - n / 4, 0L);
+  target.T = max(counts[n].T - n / 4, 0L);
+  
+  // If no change necessary, return 0
+  if (target.A == n / 4 &&
+      target.C == n / 4 &&
+      target.G == n / 4 &&
+      target.T == n / 4)
+    result = 0;
+  // Else, compute minimum substring that contains
+  // values to be changed (target count)
+  else {
+    for (long left = 1; left <= n; left++) {
+      while ((counts[right] - counts[left - 1]) < target && 
+             right < n)
+        right++;
+      
+      if (! ((counts[right] - counts[left - 1]) < target) )
+        result = min(right - left + 1, result);
+    }
+  }
+  
+  return result;
+}
+
+int main() {
+  long n;
+  string gene;
+  
+  cin >> n >> gene;
+  cout << ShortestModification(gene, n);
+}
